abd3e96b3f87947750dd3b5135c5ffadc2f8492d
thesebonesalone <will.avery21@gmail.com>
2022-01-12 09:36:26 -0600

    Added routing setup and some Sass changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
+import login_icon from './Icons/login_icon.png'
 import './App.css';
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import Signup from './Signup/Signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup/Signup.js';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './scss/custom.scss';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar expand="lg" variant="light" bg="dark">
+      <Navbar expand="lg" variant="light" bg="primary">
         <Container>
-          <Navbar.Brand href="#">Test</Navbar.Brand>
-          
+          <Navbar.Brand href="#">Meal Prep App</Navbar.Brand>
+          <Navbar.Brand href="/signup">
+            <img src = {login_icon} width="30" height="30" />
+          </Navbar.Brand>
         </Container>
       </Navbar>
-      <div>
-        <Signup></Signup>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-export const site = "http://localhost:5000"
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import { Form, Button, Alert } from "react-bootstrap";
-import { site } from "../App";
+import { site } from "../../site";
 
 
 function Signup(props) {
